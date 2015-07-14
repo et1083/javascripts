d2e69c037520842da88372a2e53db99654d0e223
et1083 <et1083@gmail.com>
2015-07-14 13:40:12 -0700

    Update famo_font.js

--- famo_font.js
+++ famo_font.js
@@ -1,1 +1,16 @@
 //Add the code you're trying to change the font with below
+var Engine = require('famous/core/Engine');
+var Surface = require('famous/core/Surface');
+
+var mainContext = Engine.createContext();
+
+var firstSurface = new Surface({
+  size: [100, 100],
+  contect: 'font',
+  properties: {
+    fontSize: "40pt",
+    backgroundColor: '#FA5C4F'
+  }
+});
+
+mainContext.add(firstSurface);
